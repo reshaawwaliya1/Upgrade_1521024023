2e0cd85dfbc7f554a52738124436088e16edb2fb
Resha Awwaliyatul Hidaya <reshaawwaliya1@gmail.com>
2016-11-22 20:53:20 +0700

    upgrade_1521024023

---
+++ UPGRADE_1521024023.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<table border="1">
+	<tr>
+		<th rowspan="2">INPUT</th>
+		<th colspan="5">OUTPUT</th>
+	</tr>
+	<tr>
+		<th>A</th>
+		<th>E</th>
+		<th>I</th>
+		<th>U</th>
+		<th>O</th>
+	</tr>
+	<tr>
+		<td>
+		<?php
+		$n1="RESHA AWWALIYATUL HIDAYAH";
+		echo $n1;
+		?>
+		</td>
+		<td>
+		<?php
+		echo substr_count($n1,"A");
+		?>
+		</td>
+		<td>
+		<?php
+		echo substr_count($n1,"E");
+		?>
+		</td>
+		<td>
+		<?php
+		echo substr_count($n1,"I");
+		?>
+		</td>
+		<td>
+		<?php
+		echo substr_count($n1,"U");
+		?>
+		</td>
+		<td>
+		<?php
+		echo substr_count($n1,"O");
+		?>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
